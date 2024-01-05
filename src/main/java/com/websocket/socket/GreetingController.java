b834bc346995734f7b0da4bc5de9c3afce68838f
최예진 <bibianabb@naver.com>
2024-01-05 18:09:08 +0900

    웹소켓 통신

--- src/main/java/com/websocket/socket/GreetingController.java
+++ src/main/java/com/websocket/socket/GreetingController.java
@@ -9,10 +9,8 @@ import org.springframework.web.util.HtmlUtils;
 public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-//    "hello" 경로로 메시지가 날아오면 greeting 메소드가 실행되어 Greeting 객체가 반환
-    public Greeting greeting(HelloMessage message) throws InterruptedException {
+    public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000);
-        return new Greeting("Hello, "+ HtmlUtils.htmlEscape(message.getName()) + "!");
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
-
 }
