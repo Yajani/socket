bb771fc6dfd45e01fa312538362e777d71645398
최예진 <bibianabb@naver.com>
2024-01-15 16:34:00 +0900

    웹소켓 EmbeddedRedisConfig redis 연결 : 채팅방 구현, user post 생성 필요

---
+++ src/main/java/com/websocket/socket/entity/Message.java
@@ -0,0 +1,46 @@
+package com.websocket.socket.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Table(name = "message")
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Message extends Timestamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name = "sender")
+    private String sender;
+
+    @Column(name = "roomId")
+    private String roomId;
+
+    @Column(name = "receiver")
+    private String receiver;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "sentTime")
+    private String sentTime;
+
+    @ManyToOne
+    @JoinColumn(name = "roomId", referencedColumnName = "roomId", insertable = false, updatable = false)
+    private MessageRoom messageRoom;
+
+    // 대화 저장
+    public Message(String sender, String roomId, String message) {
+        super();
+        this.sender = sender;
+        this.roomId = roomId;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/websocket/socket/entity/User.java
@@ -0,0 +1,4 @@
+package com.websocket.socket.entity;
+
+public class User {
+}
