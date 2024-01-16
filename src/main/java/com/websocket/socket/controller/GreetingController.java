f342b4a70bcdcdfbea3a056f8d1780536bdbc0e1
최예진 <bibianabb@naver.com>
2024-01-16 18:30:54 +0900

    mongo 연결중

--- src/main/java/com/websocket/socket/controller/GreetingController.java
+++ src/main/java/com/websocket/socket/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.websocket.socket.controller;
 
 import com.websocket.socket.Greeting;
 import com.websocket.socket.HelloMessage;
+import com.websocket.socket.entity.User_mongo;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,20 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 
 public class GreetingController {
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public Greeting greeting(HelloMessage message) throws Exception {
+//        Thread.sleep(1000);
+//        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+//    }
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
+    public Greeting greeting(User_mongo message) throws Exception {
         Thread.sleep(1000);
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
+
+
+
 }
